d160544b601336914ad685cb8d818dd908326cc8
WanyingBEI <wanying.bei@u-psud.fr>
2017-10-29 15:54:12 +0100

    ajout de codes

--- bd.pl
+++ bd.pl
@@ -6,7 +6,7 @@
 #------------------------------------------------------
 
 use DBI;
-use uft8;
+#use uft8;
 use DBD::mysql;
 use vars qw/ $VERSION /;
 $VERSION='1.0';
@@ -16,10 +16,12 @@ $VERSION='1.0';
 my $bd ='ProjetWeb2017';
 my $serveur='localhost';
 my $identifiant='root';
-my $password = 'Zneio9W:=K12';
+#my $password = 'Zneio9W:=K12';
+my $password = 'beike_mysql';
 my $port='';
 
 #Connection a la bd-----------------------------------------
+	
 print "Connexion à la base de données $bd\n";
 my $dbh= DBI->connect( "DBI:mysql:database=$bd;host=$serveur;port=$port",
 	$identifiant, $password, {
@@ -39,7 +41,7 @@ CREATE TABLE Enzyme (
 	PRIMARY KEY (num_EC)
 )
 SQL
-$dbh->do('DROP TABLE IF EXISTS Enzyme;') or die "Impossible de supprimer la table Enzyme\n\n";
+#$dbh->do('DROP TABLE IF EXISTS Enzyme;') or die "Impossible de supprimer la table Enzyme\n\n";
 $dbh->do($sql_create_table_enzyme) or die "Impossible de créer la table Enzyme";
 
 print "Creation de la table Disease";
@@ -49,7 +51,7 @@ CREATE TABLE Disease (
 	PRIMARY KEY (disease_name)
 )
 SQL
-$dbh->do('DROP TABLE IF EXISTS Disease;') or die "Impossible de supprimer la table Disease\n\n";
+#$dbh->do('DROP TABLE IF EXISTS Disease;') or die "Impossible de supprimer la table Disease\n\n";
 $dbh->do($sql_create_table_disease) or die "Impossible de créer la table Disease";
 
 print "Creation de la table Publication";
@@ -65,7 +67,7 @@ CREATE TABLE Publication (
 	PRIMARY KEY (titre,auteurs)
 )
 SQL
-$dbh->do('DROP TABLE IF EXISTS Publication;') or die "Impossible de supprimer la table Publication\n\n";
+#$dbh->do('DROP TABLE IF EXISTS Publication;') or die "Impossible de supprimer la table Publication\n\n";
 $dbh->do($sql_create_table_publication) or die "Impossible de créer la table Publication";
 
 
@@ -78,7 +80,7 @@ CREATE TABLE Names (
 	PRIMARY KEY (accepted_name)
 )
 SQL
-$dbh->do('DROP TABLE IF EXISTS Names;') or die "Impossible de supprimer la table Names\n\n";
+#$dbh->do('DROP TABLE IF EXISTS Names;') or die "Impossible de supprimer la table Names\n\n";
 $dbh->do($sql_create_table_names) or die "Impossible de créer la table Names";
 
 
@@ -90,7 +92,7 @@ CREATE TABLE ProteinFamilie (
 	PRIMARY KEY (SP)
 )
 SQL
-$dbh->do('DROP TABLE IF EXISTS ProteinFamilie;') or die "Impossible de supprimer la table ProteinFamilie\n\n";
+#$dbh->do('DROP TABLE IF EXISTS ProteinFamilie;') or die "Impossible de supprimer la table ProteinFamilie\n\n";
 $dbh->do($sql_create_table_protein) or die "Impossible de créer la table ProteinFamilie";
 
 print "Creation de la table impliqueDisease";
@@ -105,7 +107,7 @@ CREATE TABLE ImpliqueDisease (
 	FOREIGN KEY (num_EC) REFERENCES Enzyme(num_EC)
 )
 SQL
-$dbh->do('DROP TABLE IF EXISTS ImpliqueDisease;') or die "Impossible de supprimer la table ImpliqueDisease\n\n";
+#$dbh->do('DROP TABLE IF EXISTS ImpliqueDisease;') or die "Impossible de supprimer la table ImpliqueDisease\n\n";
 $dbh->do($sql_create_table_implique_disease) or die "Impossible de créer la table ImpliqueDisease";
 
 
@@ -118,12 +120,39 @@ CREATE TABLE Ecrire (
 	titre VARCHAR(100) NOT NULL,
 	auteurs VARCHAR(100) NOT NULL,
 	PRIMARY KEY (titre,auteurs,num_EC),
-	FOREIGN KEY (titre) REFERENCES Publication(titre),
-	FOREIGN KEY (auteurs) REFERENCES Publication(auteurs),
+	FOREIGN KEY (titre, auteurs) REFERENCES Publication(titre, auteurs),
 	FOREIGN KEY (num_EC) REFERENCES Enzyme(num_EC)
 )
 SQL
-$dbh->do('DROP TABLE IF EXISTS Ecrire;') or die "Impossible de supprimer la table Ecrire\n\n";
+#$dbh->do('DROP TABLE IF EXISTS Ecrire;') or die "Impossible de supprimer la table Ecrire\n\n";
 $dbh->do($sql_create_table_ecrire) or die "Impossible de créer la table Ecrire";
+
+print "Creation de la table Appartient";
+my $sql_create_table_appartient= <<"SQL";
+
+CREATE TABLE Appartient (
+	num_EC VARCHAR(20) NOT NULL,
+	SP VARCHAR(10) NOT NULL,
+	PRIMARY KEY (num_EC,SP),
+	FOREIGN KEY (num_EC) REFERENCES Enzyme(num_EC),
+	FOREIGN KEY (SP) REFERENCES Enzyme(ProteinFamilie)
+)
+SQL
+#$dbh->do('DROP TABLE IF EXISTS Appartient;') or die "Impossible de supprimer la table Appartient\n\n";
+$dbh->do($sql_create_table_appartient) or die "Impossible de créer la table Appartient";
+
+print "Creation de la table PossedeNom";
+my $sql_create_table_PossedeNom= <<"SQL";
+
+CREATE TABLE PossedeNom (
+	num_EC VARCHAR(20) NOT NULL,
+	accepted_name VARCHAR(50) NOT NULL,
+	PRIMARY KEY (num_EC,accepted_name),
+	FOREIGN KEY (num_EC) REFERENCES Enzyme(num_EC),
+	FOREIGN KEY (accepted_name) REFERENCES Enzyme(Names)
+)
+SQL
+#$dbh->do('DROP TABLE IF EXISTS PossedeNom;') or die "Impossible de supprimer la table PossedeNom\n\n";
+$dbh->do($sql_create_table_PossedeNom) or die "Impossible de créer la table PossedeNom";
 
 #Lecture des fichiers et insertions des données-----------------------------------------
