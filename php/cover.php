a2dfe9b9c8ebf6c20c6e5b87674edc9e1142e81c
MathildeBertrand <b.mathibert@gmail.com>
2017-12-02 19:27:08 +0100

    connection interface avec bd

---
+++ php/cover.php
@@ -0,0 +1,94 @@
+<?php
+
+///////////////////////////////////////////////////////////////////////////////
+//Fichier qui permet de realiser linterface du site Web////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+	
+?>
+
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title> Home page </title>
+		<link rel="stylesheet" href="css/bootstrap.min.css" />
+		<link rel="stylesheet" href="css/MyStylesheet.css" />
+	</head>
+	
+	<body class="bg">
+			<nav class="navbar navbar-default">
+				<div class="container-fluid">
+				
+					<!-- website name -->
+					<div class="navbar-header">
+						<a href="#" class="navbar-brand">ENZyclopédia</a>
+					</div>
+				
+					<!-- Menu items -->
+					<div>
+						<ul class="nav navbar-nav">
+							<li class="active"><a href="cover.php">Home</a></li>
+							<li><a href="#">About us</a></li>
+							<li><a href="#">Exploration BD</a></li>
+							<li><a href="#">FAQ</a></li>
+							<li><a href="#">Contact</a></li>
+						</ul>	
+						<ul class="nav navbar-nav navbar-right">
+							<li><a href="#"><span class="glyphicon glyphicon-user"></span> log in
+						</ul>
+					</div>
+					
+				</div>
+			</nav>
+		
+			<div class="container-fluid">
+				<div class="row">
+					
+					<div class="col-sm-3 col-md-8 col-lg-12"></div>
+						<div class="jumbotron1">
+							
+							<div class="row">
+								<div class="col-sm-3 col-md-8 col-lg-12">
+									<h1><center>ENZyclopédia</center></h1>
+									<p><center>Voici une petite intro</center></p>
+								</div>
+							</div>
+							
+							<div class="row">
+								<div class="col-sm-3 col-md-8 col-lg-12">
+									<FORM>
+									<center>
+										<!-- La liste deroulante -->
+									<SELECT name="nom" size="1" id="liste" onChange="javascript:document.getElementById('liste').value">
+									<OPTION>EC
+									<OPTION>Cofactor
+									<OPTION>Disease
+									<OPTION>Protein family
+									<OPTION>Names
+								
+									</SELECT>	
+									
+									
+									<!-- Le champ de texte -->
+									<input type="text" name="enter" id="txt" size="60"/> 
+									
+								
+									
+									<!-- Gestion des requetes a la bd -->
+									
+									<a href="javascript:open('fiche1.php?val='+document.getElementById('txt').value +' &type=' +document.getElementById('liste').value,'_self')"><input type="BUTTON" value="Search"/></a>
+									</form?>
+									</center>
+									</FORM>
+								</div>
+							</div>
+						
+						</div>
+					</div>
+				
+				</div>
+			</div>
+	
+	</body>
+</html>
--- php/fiche1.php
+++ php/fiche1.php
@@ -1,18 +1,53 @@
+<!DOCTYPE html>
+
+<!-- Mise en page -->
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title> Home page </title>
+		<link rel="stylesheet" href="css/bootstrap.min.css" />
+		<link rel="stylesheet" href="css/MyStylesheet.css" />
+	</head>
+	
+	<body class="bg">
+			<nav class="navbar navbar-default">
+				<div class="container-fluid">
+				
+					<!-- website name -->
+					<div class="navbar-header">
+						<a href="#" class="navbar-brand">ENZyclopédia</a>
+					</div>
+					
+					<!-- Menu items -->
+					<div>
+						<ul class="nav navbar-nav">
+							<li class="active"><a href="cover.php">Home</a></li>
+							<li><a href="#">About us</a></li>
+							<li><a href="ExplorationBD.php">Exploration BD</a></li>
+							<li><a href="#">FAQ</a></li>
+							<li><a href="#">Contact</a></li>
+						</ul>	
+						<ul class="nav navbar-nav navbar-right">
+							<li><a href="#"><span class="glyphicon glyphicon-user"></span> log in </a>
+						</ul>
+					</div>
+
+		</div>
+	
+	</body>	
 <?php
 require("functions.php");
 $AFF=FALSE; 
 
 ///////////////////////////////////////////////////////////////////////////////
-//Construction des requetes en fonction des resultats//////////////////////////
+//Construction des requetes qui interrogent la bdC/////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////
-
 
 //Connection a la base : 
 mysql_close;
 try
 	{
 	$bd=new PDO('mysql:host=127.0.0.1; dbname=ProjetWeb2017','root','Pachadu92');
-	echo "Connexion reussie a Mysql";
 	
 	}
 	catch(Exception $e){
@@ -22,127 +57,182 @@ try
 
 
 
-//Afficher la fiche descriptive de lenzyme selectionnee :
-
-//Resume sur lenzyme ----------------------------------------------------------------------------------------------------------------------
-	$response=Excecuter($bd,"SELECT Enzyme.o_name,Enzyme.num_EC,Enzyme.accepted_name FROM Enzyme WHERE Enzyme.num_EC='".$_GET['num_EC']."'");	
- 
+///////////////////////Si on demande un enzyme////////////////////////////////////////////////////////////////
 	
-	while($data =$response->fetch()){ //On boucle pour recuperer o_name,num_EC et accpeted_name
-				?>
-				<p style="text-align:center">
-					<strong><FONT size="6">ENZYME</strong> : <?php echo $data['num_EC']; ?></FONT><br><br>
-				</p>
-				
-				<p
-					<UL TYPE="sqare">
-					<LI><FONT color="#8B0000"><strong>Abstract</UL></FONT></strong><br>
-					<u>accepted_name</u> : <?php echo $data['accepted_name']; ?> 
-					<u>o_name</u> : <?php echo $data['o_name']; ?>
-					<u>synonym</u> :
+	if($_GET['type']=='EC'){ 
+		
+		if((stripos($_GET['val'], 'EC') !== FALSE) ){ //On test le nom de lenzyme (si contient EC ou pas)
+			$response=Excecuter($bd,"SELECT Enzyme.o_name,Enzyme.num_EC,Enzyme.accepted_name FROM Enzyme WHERE Enzyme.num_EC='".$_GET['val']."'");
+		}else{
+			$response=Excecuter($bd,"SELECT Enzyme.o_name,Enzyme.num_EC,Enzyme.accepted_name FROM Enzyme WHERE Enzyme.num_EC='EC ".$_GET['val']."'");
+		}	
+	
+
+		//Resume sur lenzyme ----------------------------------------------------------------------------------------------------------------------
+		while($data =$response->fetch()){ //On boucle pour recuperer o_name,num_EC et accpeted_name
+					?>
+					<p style="text-align:center">
+						<strong><FONT size="6">ENZYME</strong> : <?php echo $data['num_EC']; ?></FONT><br><br>
+					</p>
+					
+					<p
+						<UL TYPE="sqare">
+						<LI><FONT color="#8B0000"><strong>Abstract</UL></FONT></strong><br>
+						<u>accepted_name</u> : <?php echo $data['accepted_name']; ?> 
+						<u>o_name</u> : <?php echo $data['o_name']; ?>
+						<u>synonym</u> :
+								
+				<?php 
 							
-			<?php 
+				}
+		
+		//Les synonyms
+		if((stripos($_GET['val'], 'EC') !== FALSE) ){	
+			$response=Excecuter($bd,"SELECT synonym_name FROM Names WHERE num_EC='". $_GET['val']."'"); 
+		}else{
+			$response=Excecuter($bd,"SELECT synonym_name FROM Names WHERE num_EC='EC ". $_GET['val']."'");
+		}
+		
+		while($data =$response->fetch(PDO::FETCH_ASSOC)){
+			?>
+			 <?php echo $data['synonym_name'];
+			 echo ", "; 
+		}	
+		
+		//Les reactions et les cofacteurs
+		if((stripos($_GET['val'], 'EC') !== FALSE) ){	
+			$response=Excecuter($bd,"SELECT reaction,cofactor FROM Enzyme WHERE num_EC='". $_GET['val']."'"); 
+		}else{
+			$response=Excecuter($bd,"SELECT reaction,cofactor FROM Enzyme WHERE num_EC='EC ". $_GET['val']."'");
+		}
+		
+		while($data =$response->fetch(PDO::FETCH_ASSOC)){
+			?>
+			<br>
+				<u>Reaction</u> : <?php echo $data['reaction'];?>  <br>
+				
+				<?php //On regarde si la chaine de cofactor est vide et si non, on ecrit les cofactor associes a lenzyme
+				$str2='';
+				if (strcmp ($data['cofactor'], $str2 )!=0){ 
+				?><u>Cofactors</u> : <?php echo $data['cofactor'];?><br>
+			<?php }
+			
+		}
+		
+		//Les commentaires	
+		if((stripos($_GET['val'], 'EC') !== FALSE) ){	
+			$response=Excecuter($bd,"SELECT comment FROM Comments WHERE num_EC='". $_GET['val']."'"); 
+		}else{
+			$response=Excecuter($bd,"SELECT comment FROM Comments WHERE num_EC='EC ". $_GET['val']."'");
+		}
+		
+		while($data =$response->fetch(PDO::FETCH_ASSOC)){
+			?>
+			
+			<br><?php //On regarde si la chaine de commentaire est vide et si non, on ecrit les comments associes a lenzyme
+			$str2='';
+			if (strcmp ($data['comment'], $str2 )!=0){ 
+				?><u>Comments</u> :<?php
+				echo $data['comment'];}?><br> 
+			<br>
+			
+			<?php
+		}	
+			
+			
+		//Lensemble des publications----------------------------------------------------------------------------------------------------------------------
+		
+		?><UL TYPE="sqare">
+		<LI><FONT color="#8B0000"><strong>Publication</UL></FONT></strong><br>
+		<?php	
+		
+		if((stripos($_GET['val'], 'EC') !== FALSE) ){				
+			$response=Excecuter($bd,"SELECT * FROM Publication WHERE num_EC='".$_GET['val']."'"); 
+		}else{
+			$response=Excecuter($bd,"SELECT * FROM Publication WHERE num_EC='EC ".$_GET['val']."'");
+		}	
+		
+		while($data =$response->fetch(PDO::FETCH_ASSOC)){
+			?>
+			
+			<u>Title</u> : <?php echo $data['titre']; ?> <br>
+			<u>authors</u> : <?php echo $data['auteurs']; 
+			
+			//On test : si on a des valeurs =0, cest que lon a pas dinformation dessus
+			if ($data['first_page'] !=0){ ?> <br> <u>first_page</u> : <?php  echo $data['first_page'];} 
+			if ($data['last_page'] !=0){ ?> <br><u>last_page</u> : <?php echo $data['last_page']; }
+			if ($data['volume'] !=0){ ?> <br><u>volume</u> : <?php echo $data['volume'];} 
+			if($data['pubmed'] !=0){ ?> <br> <u>pubmed</u> : <a href="https://www.ncbi.nlm.nih.gov/pubmed/?term=<?php echo $data['pubmed'];?>"><?php echo $data['pubmed']; ?></a>  <?php }
+
+			 if($data['year'] !=0){ ?> <br><u>year</u> : <?php echo $data['year'];} ?> <br><br>
+			
+			<?php
+		}
+		
+		//History----------------------------------------------------------------------------------------------------------------------
+		
+		if((stripos($_GET['val'], 'EC') !== FALSE) ){	
+			$response=Excecuter($bd,"SELECT history FROM Enzyme WHERE num_EC='".$_GET['val']."'"); 
+		}else{
+			$response=Excecuter($bd,"SELECT history FROM Enzyme WHERE num_EC='EC ".$_GET['val']."'");
+		}
+		
+		while($data =$response->fetch(PDO::FETCH_ASSOC)){
+		?>
+		 <UL TYPE="sqare">
+		<LI><FONT color="#8B0000"><strong>History</UL></FONT></strong><br>
+		<?php echo $data['history']; 
+
+		}
+		
+		//Les Sequences proteiques--------------------------------------------------------------------------------------------------------
+		
+		?><UL TYPE="sqare">
+		<LI><FONT color="#8B0000"><strong>Protein Sequence</UL></FONT></strong><br>
+		<u>SP</u> :
+		<?php
+		
+		if((stripos($_GET['val'], 'EC') !== FALSE) ){	
+			$response=Excecuter($bd,"SELECT SP_name,SP_id FROM ProtSeq WHERE num_EC='".$_GET['val']."'"); 
+		}else{
+			$response=Excecuter($bd,"SELECT SP_name,SP_id FROM ProtSeq WHERE num_EC='EC ".$_GET['val']."'");
+		}
+		while($data =$response->fetch(PDO::FETCH_ASSOC)){
+			?>
+			 <a href="http://www.uniprot.org/uniprot/<?php echo $data['SP_id'];?>">	<?php echo $data['SP_name']; echo " ; ";?></a>  <?php
+		
+		}
+		
+		//Les Familles proteiques--------------------------------------------------------------------------------------------------------
+		
+		?><UL TYPE="sqare">
+			<LI><FONT color="#8B0000"><strong>Protein Family</UL></FONT></strong><br>`
+			<u>PROSITE</u> :
+		<?php
+		
+		if((stripos($_GET['val'], 'EC') !== FALSE) ){	
+			$response=Excecuter($bd,"SELECT PROSITE FROM Family WHERE num_EC='".$_GET['val']."'"); 
+		}else{
+			$response=Excecuter($bd,"SELECT PROSITE FROM Family WHERE num_EC='EC ".$_GET['val']."'");
+		}	
+		
+		while($data =$response->fetch(PDO::FETCH_ASSOC)){
+			?>
+			<a href="https://prosite.expasy.org/<?php echo $data['PROSITE'];?>">	<?php echo $data['PROSITE']; echo " ; ";?></a>  <?php
+		}
+	
+	
+//////////////////////////Si on demande un cofacteur////////////////////////////////////////////////////////////////
+	
+	}else if ($_GET['type']=='Cofactor'){ 
+			$response=Excecuter($bd,"Select num_EC FROM Enzyme WHERE cofactor='".$_GET['val']."'");
+			
+			?><strong><FONT size="6">Liste des enzymes qui utilisent le cofacteur <?php echo $_GET['val']?></strong><br><br> <?php
+			
+			while($data =$response->fetch(PDO::FETCH_ASSOC)){ //Si on clique sur une enzyme, on peut avoir sa fiche descriptive
+				?><strong><FONT size="6">ENZYME</strong> :<a href="fiche1.php?val=<?php echo $data['num_EC']; ?> &type=EC"> <?php echo $data['num_EC']; ?></a></FONT><br><?php 
 						
 			}
-			
-	$response=Excecuter($bd,"SELECT synonym_name FROM Names WHERE num_EC='". $_GET['num_EC']."'"); //Les synonyms
-	while($data =$response->fetch(PDO::FETCH_ASSOC)){
-		?>
-		 <?php echo $data['synonym_name'];
-		 echo ", "; 
-	}	
-	
-	$response=Excecuter($bd,"SELECT reaction,cofactor FROM Enzyme WHERE num_EC='". $_GET['num_EC']."'"); //Les reactions et les cofacteurs
-	while($data =$response->fetch(PDO::FETCH_ASSOC)){
-		?>
-		<br>
-			<u>Reaction</u> : <?php echo $data['reaction'];?>  <br>
-			
-			<?php //On regarde si la chaine de cofactor est vide et si non, on ecrit les cofactor associes a lenzyme
-			$str2='';
-			if (strcmp ($data['cofactor'], $str2 )!=0){ 
-			?><u>Cofactors</u> : <?php echo $data['cofactor'];?><br>
-		<?php }
-		
-	}
-	
-	$response=Excecuter($bd,"SELECT comment FROM Comments WHERE num_EC='". $_GET['num_EC']."'"); //Les commentaires	
-	while($data =$response->fetch(PDO::FETCH_ASSOC)){
-		?>
-		
-		<br><?php //On regarde si la chaine de commentaire est vide et si non, on ecrit les comments associes a lenzyme
-		$str2='';
-		if (strcmp ($data['comment'], $str2 )!=0){ 
-			?><u>Comments</u> :<?php
-			echo $data['comment'];}?><br> 
-		<br>
-		
-		<?php
-	}	
-		
-		
-	//Lensemble des publications----------------------------------------------------------------------------------------------------------------------
-	
-	?><UL TYPE="sqare">
-	<LI><FONT color="#8B0000"><strong>Publication</UL></FONT></strong><br>
-	<?php	
-				
-	$response=Excecuter($bd,"SELECT * FROM Publication WHERE num_EC='".$_GET['num_EC']."'"); 	
-	while($data =$response->fetch(PDO::FETCH_ASSOC)){
-		?>
-		
-		<u>Title</u> : <?php echo $data['titre']; ?> <br>
-		<u>authors</u> : <?php echo $data['auteurs']; 
-		
-		//On test : si on a des valeurs =0, cest que lon a pas dinformation dessus
-		if ($data['first_page'] !=0){ ?> <br> <u>first_page</u> : <?php  echo $data['first_page'];} 
-		if ($data['last_page'] !=0){ ?> <br><u>last_page</u> : <?php echo $data['last_page']; }
-		if ($data['volume'] !=0){ ?> <br><u>volume</u> : <?php echo $data['volume'];} 
-		if($data['pubmed'] !=0){ ?> <br> <u>pubmed</u> : <a href="https://www.ncbi.nlm.nih.gov/pubmed/?term=<?php echo $data['pubmed'];?>"><?php echo $data['pubmed']; ?></a>  <?php }
-
-		 if($data['year'] !=0){ ?> <br><u>year</u> : <?php echo $data['year'];} ?> <br><br>
-		
-		<?php
-	}
-	
-	//History----------------------------------------------------------------------------------------------------------------------
-	
-	$response=Excecuter($bd,"SELECT history FROM Enzyme WHERE num_EC='".$_GET['num_EC']."'"); 
-	while($data =$response->fetch(PDO::FETCH_ASSOC)){
-	?>
-	 <UL TYPE="sqare">
-	<LI><FONT color="#8B0000"><strong>History</UL></FONT></strong><br>
-	<?php echo $data['history']; 
-
-	}
-	
-	//Les Sequences proteiques--------------------------------------------------------------------------------------------------------
-	
-	?><UL TYPE="sqare">
-	<LI><FONT color="#8B0000"><strong>Protein Sequence</UL></FONT></strong><br>
-	<u>SP</u> :
-	<?php
-	
-	$response=Excecuter($bd,"SELECT SP_name,SP_id FROM ProtSeq WHERE num_EC='".$_GET['num_EC']."'"); 
-	while($data =$response->fetch(PDO::FETCH_ASSOC)){
-		?>
-		 <a href="http://www.uniprot.org/uniprot/<?php echo $data['SP_id'];?>">	<?php echo $data['SP_name']; echo " ; ";?></a>  <?php
-	
-	}
-	
-	//Les Familles proteiques--------------------------------------------------------------------------------------------------------
-	
-	?><UL TYPE="sqare">
-		<LI><FONT color="#8B0000"><strong>Protein Family</UL></FONT></strong><br>`
-		<u>PROSITE</u> :
-	<?php
-	
-	$response=Excecuter($bd,"SELECT PROSITE FROM Family WHERE num_EC='".$_GET['num_EC']."'"); 
-	while($data =$response->fetch(PDO::FETCH_ASSOC)){
-		?>
-		<a href="https://prosite.expasy.org/<?php echo $data['PROSITE'];?>">	<?php echo $data['PROSITE']; echo " ; ";?></a>  <?php
-		 
-	
 	}
 
 ?>
